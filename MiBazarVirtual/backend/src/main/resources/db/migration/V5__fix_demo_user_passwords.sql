67c9b6b7f230d737a46eff9a04f854f099bf11e1
Dany Velásquez <dannysv.777@gmail.com>
2026-05-13 19:30:46 -0600

    Add demo order testing collection

---
+++ MiBazarVirtual/backend/src/main/resources/db/migration/V5__fix_demo_user_passwords.sql
@@ -0,0 +1,13 @@
+UPDATE users
+SET password = '$2a$10$QQGaBbfOCUdGdBWJbhU0bOvV71aNVcqeNIvC3UH.eajnKkBRVrzXC'
+WHERE email IN (
+    'admin@mibazarvirtual.com',
+    'pedro.gonzalez@gmail.com',
+    'rosa.martinez@gmail.com',
+    'rancho.verde@gmail.com',
+    'sanjose.pan@gmail.com',
+    'altiplano.lacteos@gmail.com',
+    'carlos.lopez@gmail.com',
+    'maria.perez@gmail.com',
+    'jose.ramos@gmail.com'
+);
